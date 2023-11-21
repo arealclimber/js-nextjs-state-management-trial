d38a4e769278bfb37ea0f464c6dff15e5448e131
arealclimber <luffulgence@gmail.com>
2023-11-21 16:13:55 +0800

    test: 💍 use store in React Context, but useless

--- context/store_context.js
+++ context/store_context.js
@@ -4,31 +4,51 @@ import { useGlobalStore } from "@/stores/zustand";
 const Context = createContext(null);
 
 const useStore = () => {
-  const [teamA, setTeamA] = useState(0);
-  const [teamB, setTeamB] = useState(0);
+  // const [teamA, setTeamA] = useState(0);
+  // const [teamB, setTeamB] = useState(0);
 
-  const { increaseTeamAScore } = useGlobalStore();
+  const [
+    teamA,
+    teamB,
+    increaseTeamAScore,
+    decreaseTeamAScore,
+    increaseTeamBScore,
+    decreaseTeamBScore,
+  ] = useGlobalStore((state) => [
+    state.teamA,
+    state.teamB,
+    state.increaseTeamAScore,
+    state.decreaseTeamAScore,
+    state.increaseTeamBScore,
+    state.decreaseTeamBScore,
+  ]);
 
-  const resetZustand = () => {
-    console.log("resetZustand called");
-    increaseTeamAScore();
-    increaseTeamAScore();
-    increaseTeamAScore();
-    increaseTeamAScore();
-    increaseTeamAScore();
-  };
+  console.log("use store in React Context 沒有用，一樣浪費效能");
 
-  useEffect(() => {
-    resetZustand();
-  }, []);
+  // const { increaseTeamAScore } = useGlobalStore();
+  // const resetZustand = () => {
+  //   console.log("resetZustand called");
+  //   increaseTeamAScore();
+  //   increaseTeamAScore();
+  //   increaseTeamAScore();
+  //   increaseTeamAScore();
+  //   increaseTeamAScore();
+  // };
+  // useEffect(() => {
+  //   resetZustand();
+  // }, []);
 
   return {
     teamA, // Team A's score
     teamB, // Team B's score
-    increaseTeamAScore: () => setTeamA((v) => v + 1), // Increase Team A's score by one
-    decreaseTeamAScore: () => setTeamA((v) => v - 1), // Decrease Team A's score by one
-    increaseTeamBScore: () => setTeamB((v) => v + 1), // Increase Team B's score by one
-    decreaseTeamBScore: () => setTeamB((v) => v - 1), // Decrease Team B's score by one
+    increaseTeamAScore,
+    decreaseTeamAScore,
+    increaseTeamBScore,
+    decreaseTeamBScore,
+    // increaseTeamAScore: () => setTeamA((v) => v + 1), // Increase Team A's score by one
+    // decreaseTeamAScore: () => setTeamA((v) => v - 1), // Decrease Team A's score by one
+    // increaseTeamBScore: () => setTeamB((v) => v + 1), // Increase Team B's score by one
+    // decreaseTeamBScore: () => setTeamB((v) => v - 1), // Decrease Team B's score by one
   };
 };
 
